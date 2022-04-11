6bfbf44ce80b872bf87fcbaccd32627d4f9867fa
Ram Baghdadi <ram_baghdadi@hotmail.com>
2022-04-11 22:02:27 +0100

    added pin functionality

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
-import { Center, Container, Space, Table, Text, TextInput } from "@mantine/core"
+import {
+	ActionIcon,
+	Center,
+	Container,
+	Loader,
+	Space,
+	Table,
+	Text,
+	TextInput,
+} from "@mantine/core"
 import { useEffect, useState } from "react"
 import { SortAscendingIcon, SortDescendingIcon } from "@heroicons/react/solid"
+import { Pinned } from "tabler-icons-react"
+import { PinnedOff } from "tabler-icons-react"
 
 export default function Home() {
 	const [countryData, setCountryData] = useState([])
 	const [ascending, setAscending] = useState(false)
 	const [ascendingPop, setAscendingPop] = useState(true)
 	const [searchValue, setSearchValue] = useState("")
+	const [pin, setPin] = useState({
+		country: {},
+		id: "",
+	})
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(false)
 
 	async function fetchData() {
 		try {
+			setError(false)
+			setLoading(true)
 			const response = await fetch(`https://restcountries.com/v3.1/all`)
 			const data = await response.json()
 			await data.map((country) => {
@@ -22,6 +41,11 @@ export default function Home() {
 							population: country.population.toLocaleString("en-US"),
 							flag: country.flag,
 							continent: country.continents,
+							id:
+								country.name.common +
+								country.capital +
+								country.population +
+								Math.floor(Math.random() * 99),
 						},
 					].sort((a, d) => {
 						if (a.name.toUpperCase() < d.name.toUpperCase()) {
@@ -34,8 +58,10 @@ export default function Home() {
 					})
 				})
 			})
+			setLoading(false)
 		} catch (error) {
 			console.error(error)
+			setError(true)
 		}
 	}
 
@@ -95,26 +121,14 @@ export default function Home() {
 		}
 	}
 
-	// function searchCountries(e) {
-	// 	let input = e.target.value.toLowerCase()
-
-	// 	if (input !== "") {
-	// 		setCountryData(
-	// 			countryData.filter((country) =>
-	// 				country.name.toLowerCase().includes(input)
-	// 			)
-	// 		)
-	// 	}
-	// 	if (input === "") {
-	// 		setCountryData([])
-	// 		fetchData()
-	// 	}
-	// }
+	function handlePin(country) {
+		console.log(country)
+		countryData.unshift(country)
+	}
 
 	return (
 		<Container style={{ marginTop: "5rem" }}>
 			<Center>
-				{" "}
 				<Text
 					style={{
 						fontSize: "3rem",
@@ -136,73 +150,119 @@ export default function Home() {
 				size="lg"
 			/>
 			<Space h="xl" />
-			<Table
-				horizontalSpacing="xl"
-				verticalSpacing="md"
-				fontSize="md"
-				striped
-				highlightOnHover
-			>
-				<thead>
-					<tr>
-						<th>
-							<div
-								style={{
-									display: "flex",
-									alignItems: "center",
-									gap: "1rem",
-								}}
-							>
-								<span>Country</span>
-								<div style={{ cursor: "pointer" }} onClick={sortAlphabetical}>
-									{ascending ? (
-										<SortAscendingIcon height={25} width={25} />
-									) : (
-										<SortDescendingIcon height={25} width={25} />
-									)}
+			<Center>
+				{loading && <Loader />}
+				{error && <p>Error! Please try again later.</p>}
+			</Center>
+			{!loading && (
+				<Table
+					horizontalSpacing="xl"
+					verticalSpacing="md"
+					fontSize="md"
+					striped
+					highlightOnHover
+				>
+					<thead>
+						<tr>
+							<th></th>
+							<th>
+								<div
+									style={{
+										display: "flex",
+										alignItems: "center",
+										gap: "1rem",
+									}}
+								>
+									<span>Country</span>
+									<div style={{ cursor: "pointer" }} onClick={sortAlphabetical}>
+										{ascending ? (
+											<SortAscendingIcon height={25} width={25} />
+										) : (
+											<SortDescendingIcon height={25} width={25} />
+										)}
+									</div>
 								</div>
-							</div>
-						</th>
-						<th>Capital</th>
-						<th>
-							<div
-								style={{
-									display: "flex",
-									alignItems: "center",
-									gap: "1rem",
-								}}
-							>
-								<span>Population</span>
-								<div onClick={sortPopulation}>
-									{ascendingPop ? (
-										<SortAscendingIcon height={25} width={25} />
-									) : (
-										<SortDescendingIcon height={25} width={25} />
-									)}
+							</th>
+							<th>Capital</th>
+							<th>
+								<div
+									style={{
+										display: "flex",
+										alignItems: "center",
+										gap: "1rem",
+									}}
+								>
+									<span>Population</span>
+									<div onClick={sortPopulation}>
+										{ascendingPop ? (
+											<SortAscendingIcon height={25} width={25} />
+										) : (
+											<SortDescendingIcon height={25} width={25} />
+										)}
+									</div>
 								</div>
-							</div>
-						</th>
-						<th>Flag</th>
-						<th>Continent</th>
-					</tr>
-				</thead>
-				<tbody>
-					{countryData &&
-						countryData
-							.filter((country) =>
-								country.name.toLowerCase().includes(searchValue)
-							)
-							.map((country) => (
-								<tr key={Math.floor(Math.random() * 9999999)}>
-									<td>{country.name}</td>
-									<td>{country.capital}</td>
-									<td>{country.population}</td>
-									<td style={{ fontSize: "4rem" }}>{country.flag}</td>
-									<td>{country.continent}</td>
-								</tr>
-							))}
-				</tbody>
-			</Table>
+							</th>
+							<th>Flag</th>
+							<th>Continent</th>
+						</tr>
+					</thead>
+					<tbody>
+						{pin.id && (
+							<tr>
+								<td
+									onClick={() => {
+										setPin(false)
+									}}
+								>
+									{
+										<ActionIcon color={"red"}>
+											<PinnedOff size={20} />
+										</ActionIcon>
+									}
+								</td>
+								<td>{pin.country.name}</td>
+								<td>{pin.country.capital}</td>
+								<td>{pin.country.population}</td>
+								<td style={{ fontSize: "4rem" }}>{pin.country.flag}</td>
+								<td>{pin.country.continent}</td>
+							</tr>
+						)}
+
+						{countryData &&
+							countryData
+								.filter(
+									(country) =>
+										country.name.toLowerCase().includes(searchValue) &&
+										country.id !== pin.id
+								)
+								.map((country) => (
+									<tr key={country.id}>
+										<td
+											onClick={() => {
+												setPin(() => {
+													return {
+														country: country,
+														id: country.id,
+													}
+												})
+											}}
+										>
+											{
+												<ActionIcon color={"blue"}>
+													<Pinned size={20} />
+												</ActionIcon>
+											}
+										</td>
+										<td>{country.name}</td>
+										<td>{country.capital}</td>
+										<td>{country.population}</td>
+										<td style={{ fontSize: "4rem" }}>{country.flag}</td>
+										<td>{country.continent}</td>
+									</tr>
+								))}
+					</tbody>
+				</Table>
+			)}
 		</Container>
 	)
 }
